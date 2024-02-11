bf92baacdc5ac63c06a3a7affe102964e585c173
MohammadTaghipoor <114229873+Mohammad6495@users.noreply.github.com>
2024-02-11 04:15:34 -0800

    whesunjkksklf

--- src/app/modules/auth/components/Login.tsx
+++ src/app/modules/auth/components/Login.tsx
@@ -80,7 +80,7 @@ export function Login() {
     >
       {/* begin::Heading */}
       <div className='text-center mb-10'>
-        <h1 className='text-dark mb-3'>ورود به پنل پردیسان</h1>
+        <h1 className='text-dark mb-3'>ورود به پنل مدیریت</h1>
       </div>
       {/* begin::Heading */}
 
